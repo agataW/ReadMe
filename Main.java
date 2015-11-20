6a40e3b3b703aa86054e5ebb1175f1fb85fc24bc
Agata <agata.wujek@gmail.com>
2015-11-20 23:28:31 +0100

    1.

---
+++ Main.java
@@ -0,0 +1,12 @@
+package pl.wujko.ReadMe;
+
+/**
+ * Created by Agata on 2015-11-20.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Pawel!");
+    }
+}
