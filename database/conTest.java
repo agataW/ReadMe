1d57055cb3425a539330f5e20c27a7c36732edee
Agata <agata.wujek@gmail.com>
2015-11-21 00:09:51 +0100

    create databaseService

--- database/conTest.java
+++
@@ -1,9 +0,0 @@
-package pl.wujko.ReadMe.database;
-
-/**
- * Created by Agata on 2015-11-20.
- */
-public class conTest
-{
-    int w,g;
-}
---
+++ database/impl/DatabaseServiceDefault.java
@@ -0,0 +1,11 @@
+package pl.wujko.ReadMe.database.impl;
+
+import pl.wujko.ReadMe.database.DatabaseService;
+
+/**
+ * Created by Agata on 2015-11-20.
+ */
+public class DatabaseServiceDefault implements DatabaseService
+{
+
+}
